7a639a53710daadaedf280d46fd42accfee645fe
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2025-03-28 09:06:01 -0400

    Made it possible to put in an answer and submit it for the check

--- Spelling/Views/QuizView.swift
+++ Spelling/Views/QuizView.swift
@@ -12,6 +12,9 @@ struct QuizView: View {
     // MARK: Stored properties
     @State var currentItem = itemsToSpell.randomElement()!
     
+    // The user's guess
+    @State var userGuess = ""
+    
     // MARK: Computed properties
     var body: some View {
         
@@ -19,8 +22,29 @@ struct QuizView: View {
             Image(currentItem.imageName)
                 .resizable()
                 .scaledToFit()
+            
+            // Allow the user to input their guess
+            TextField("Enter the name of the item", text: $userGuess)
+            
+            // Make it possible to check the guess made
+            Button {
+                checkGuess()
+            } label: {
+                Text("Submit")
+            }
+
         }
         
+    }
+    
+    // MARK: Functions
+    func checkGuess() {
+        
+        if userGuess == currentItem.word {
+            print("Correct")
+        } else {
+            print("Incorrect")
+        }
     }
 }
 
